a658b2613b9b5103b8b38d09995d84e137cb5cbf
Mason Stewart <masondesu@gmail.com>
2013-04-30 13:17:27 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Batchmaker::Application.config.secret_token = '72fb9ed91dafa9582d7e61bb71804fe83faa24cbdd793949b3474da9b644f18c1c14a4e879eef17cbdd8a0caeac4ed73d99615099d158e257877a6bb618ec810'
