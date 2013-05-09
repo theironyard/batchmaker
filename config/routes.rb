9331990c85afbbaadeb3b669ee8f51e44944aecc
Rasheeda Mickel <gvldesigner@hotmail.com>
2013-05-09 13:47:51 -0400

    Adding home page files

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Batchmaker::Application.routes.draw do
   get 'recipe_preview' => 'pages#recipe_preview'
   get 'newrecipe' => 'pages#newrecipe'
   get 'dropdown' => 'pages#dropdown'
+  get 'recipe_images_page' => 'pages#recipe_images_page'
 end
