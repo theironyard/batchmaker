4e7ea83a591477f8f478297fe6042da23b8e5474
Mason Stewart <masondesu@gmail.com>
2013-06-25 15:23:28 -0400

    Further defined relationships between steps and recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -9,12 +9,19 @@ class Recipe < ActiveRecord::Base
     :prep_time,
     :public,
     :name,
-    :type,
+    :category,
     :yield_amount,
     :yield_name,
-    :user_id
+    :user_id,
+    :steps_attributes,
+    :ingredient_amounts_attributes
   )
 
   belongs_to :user
   has_many :steps
+  # has_many :ingredient_amounts, :through => :steps
+
+  accepts_nested_attributes_for :steps, :allow_destroy => true
+  # accepts_nested_attributes_for :ingredient_amounts, :allow_destroy => true
+
 end
--- app/models/step.rb
+++ app/models/step.rb
@@ -1,6 +1,9 @@
 class Step < ActiveRecord::Base
-  attr_accessible :directions, :recipe_id
+  attr_accessible :directions, :recipe_id, :ingredient_amounts_attributes
 
   belongs_to :recipe
   has_many :ingredient_amounts
+
+  accepts_nested_attributes_for :ingredient_amounts, :allow_destroy => true
+
 end
